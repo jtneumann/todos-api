528141120faa5cecd42e98e4820c2a9f1c545611
jtn <jtneumann@hotmail.com>
2018-08-10 21:56:37 +0000

    initial commit includes:
    api and front-end only minor updates remaining

---
+++ public/app.js
@@ -0,0 +1,62 @@
+/* global $ */
+$(document).ready(function(){
+    $.getJSON('/api/todos')
+    .then(addTodos);
+    
+    $('#todoInput').keypress(function(event){
+        if(event.which == 13){
+            createTodo();
+        }
+    });
+    
+    $('.list').on('click', 'span', function(){
+        removeTodo($(this).parent());
+    });
+});
+
+
+
+function addTodos(todos){
+    //add todos to page here
+    todos.forEach(function(todo){
+        addTodo(todo);
+    });
+}
+
+function addTodo(todo){
+        var newTodo = $('<li class="task">' + todo.name + '<span>X</span></li>');
+        newTodo.data('id',todo._id);
+        if(newTodo.completed){
+            todo.addClass("done");
+        }
+        $('.list').append(newTodo);
+}
+
+
+function createTodo(){
+    //send request to create new todo
+    var usrInput = $('#todoInput').val();
+    $.post('/api/todos/', {name: usrInput})
+    .then(function(newTodo){
+        $('#todoInput').val('');
+        addTodo(newTodo);
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+}
+
+function removeTodo(todo){
+        var clickedId = todo.data('id');
+        var deleteUrl = '/api/todos/' + clickedId;
+        $.ajax({
+            method: 'DELETE',
+            url: deleteUrl
+        })
+        .then((data)=>{
+            todo.remove();
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+}
